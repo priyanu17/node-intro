7a04036aef98509ce9f1b1f34ca4dc4ec238a282
priyanu17 <anu396priya@gmail.com>
2023-02-02 13:17:45 +0530

    asynchronous file read, write in node

--- index.js
+++ index.js
@@ -2,10 +2,24 @@
 
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// reading and writing code in a synchronous manner
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is our available information on Avocados. ${textIn}. Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
 
-console.log(textIn);
+// Non blocking asynchronous code for reading and writing to files
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  console.log(data1);
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log(data3);
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('Your file has bee written');
+      });
+    });
+  });
+});
 
-const textOut = `This is our available information on Avocados. ${textIn}. Created on ${Date.now()}`;
-
-fs.writeFileSync('./txt/output.txt', textOut);
+console.log('After read file');
